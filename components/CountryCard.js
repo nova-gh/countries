1715da1a2fa5cf71950cd49c07593b6747c857fd
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-03 11:22:17 -0800

    Update CountryCard.js

--- components/CountryCard.js
+++ components/CountryCard.js
@@ -7,7 +7,7 @@ const CountryCard = ({ flagImg, countryName, pop, capital, region }) => {
 
   return (
     <div
-      className={`flex flex-col m-2   
+      className={`flex flex-col m-6 lg:m-0
       ${
         darkMode == false
           ? "bg-[#2A3642] text-white"
