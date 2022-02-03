d655dcdd4093362f4c9ef87a0166c3f813defdfd
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-03 12:46:55 -0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,24 +7,28 @@ const Navbar = () => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
   return (
     <div
-      className={`w-full mx-auto ${
+      className={`w-full mx-auto px-4 ${
         darkMode == false
           ? "bg-[#2A3642] text-white"
           : "bg-gray-200 text-gray-900"
       }`}
     >
-      <nav className="container flex items-center justify-between px-4 py-6 mx-auto md:px-0">
+      <nav className="container flex items-center justify-between px-4 py-6 mx-auto lg:px-0">
         <div className="flex items-center ">
           <FcGlobe className="text-2xl" />
-          <h1>Countries</h1>
+          <h1 className="select-none">Countries</h1>
         </div>
-        <div className="flex items-center space-x-4">
+        <div
+          className="flex items-center space-x-4 cursor-pointer"
+          onClick={() => setDarkMode(!darkMode)}
+        >
           {darkMode == false ? (
             <MdOutlineLightMode className="text-lg" />
           ) : (
             <MdOutlineDarkMode className="text-lg" />
           )}
-          <button onClick={() => setDarkMode(!darkMode)}>Mode</button>
+          <h2 className="select-none">Mode</h2>
+          {/* <button onClick={() => setDarkMode(!darkMode)}>Mode</button> */}
         </div>
       </nav>
     </div>
