437015ba512f0e3ce4013d3b539b6488b4872928
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-03 22:17:29 -0800

    Update CountryCard.js

--- components/CountryCard.js
+++ components/CountryCard.js
@@ -7,29 +7,29 @@ const CountryCard = ({ flagImg, countryName, pop, capital, region, id }) => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
 
   return (
-    <Link href={`/country/${cid}`} passHref>
-      <div
-        className={`flex flex-col m-6 lg:m-0 shadow-lg cursor-pointer
+    // <Link href={`/country/${cid}`} passHref>
+    <div
+      className={`flex flex-col m-6 lg:m-0 shadow-lg cursor-pointer
       ${
         darkMode == false
           ? "bg-[#2A3642] text-white"
           : "bg-gray-200 text-gray-900"
       }
     `}
-      >
-        <div className="h-40 ">
-          <img src={flagImg} className="object-cover w-full h-full " />
-        </div>
-        <div className="flex flex-col items-start p-4 mt-4 text-sm">
-          <h1 className="font-semibold ">{countryName}</h1>
-          <div className="pt-4">
-            <h2 className="">Population: {pop}</h2>
-            <h2 className="">Region: {region}</h2>
-            {capital === null ? "" : <h2 className="">Capital: {capital}</h2>}
-          </div>
+    >
+      <div className="h-40 ">
+        <img src={flagImg} className="object-cover w-full h-full " />
+      </div>
+      <div className="flex flex-col items-start p-4 mt-4 text-sm">
+        <h1 className="font-semibold ">{countryName}</h1>
+        <div className="pt-4">
+          <h2 className="">Population: {pop}</h2>
+          <h2 className="">Region: {region}</h2>
+          {capital === null ? "" : <h2 className="">Capital: {capital}</h2>}
         </div>
       </div>
-    </Link>
+    </div>
+    // </Link>
   );
 };
 
