b3e9d925a27ca8b00de087c8e6d5591c20f14f3f
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-08 17:45:54 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&family=Raleway:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
