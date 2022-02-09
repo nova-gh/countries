5e34c99a5abf050e049eba977a1ab7502b327a06
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-08 17:14:25 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import { ThemeContext } from "../helper/Context";
 import { useState } from "react";
+import Head from "next/head";
 const Layout = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
   return (
@@ -12,6 +13,26 @@ const Layout = ({ children }) => {
             : "text-gray-900 bg-white"
         }`}
       >
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="View countries facts around the world"
+          ></meta>
+          <meta
+            property="og:title"
+            content="Countries facts and details"
+            key="ogtitle"
+          />
+          <meta
+            property="og:description"
+            content="Learn about countries around the world and key facts about them."
+            key="ogdesc"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Countries facts and details</title>
+        </Head>
         <Navbar />
         {children}
       </div>
