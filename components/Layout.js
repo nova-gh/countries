166f7635e68f8944961abf0114556b478ac01f6d
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-09 20:35:47 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,24 @@ const Layout = ({ children }) => {
             content="Learn about countries around the world and key facts about them."
             key="ogdesc"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
           <title>Countries facts and details</title>
         </Head>
         <Navbar />
