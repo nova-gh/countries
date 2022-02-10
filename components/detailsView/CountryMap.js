8eb3869be758fe37df47f9d73b2859759c080f62
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-09 19:32:54 -0800

    Create CountryMap.js

---
+++ components/detailsView/CountryMap.js
@@ -0,0 +1,29 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
+const CountryMap = ({ latLong, countryName }) => {
+  const icon = L.icon({ iconUrl: "/mapAssets/marker-icon.png" });
+
+  const [zoom, setZoom] = useState(7);
+  const position = latLong;
+  useEffect(() => {}, [zoom]);
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={false}
+      className="rounded-2xl"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={icon}>
+        <Popup>{countryName}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default CountryMap;
