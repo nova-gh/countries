60f29713bf3b4c1ec3af1d049b8c9d6c12c9b233
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2022-02-10 13:52:00 -0800

    added basic animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,45 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideRight: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        slideLeft: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(50%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        scaleInCenter: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        slideRight: "slideRight 1s ease-in-out",
+        slideLeft: "slideLeft 1s ease-in-out",
+        scaleInCenter: "scaleInCenter 1s ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
